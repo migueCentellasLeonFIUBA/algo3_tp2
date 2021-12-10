1e462eeb76fc75670785e370ea8fd072208a6338
Nico-Fer <fernandeznicolas1801@gmail.com>
2021-12-10 11:32:24 -0300

    Eliminacion carpeta Pistas

--- src/main/test/CasosDeUso/CasoDeUsoCuatroTest.java
+++ src/main/test/CasosDeUso/CasoDeUsoCuatroTest.java
@@ -19,7 +19,7 @@ public class CasoDeUsoCuatroTest {
 
     @Test
     public void casoDeUsoCuatro() throws Exception {
-
+/*
         List<Edificio> listaEdificios = new ArrayList<>();
 
         //Juego juego = new Juego();
@@ -58,6 +58,8 @@ public class CasoDeUsoCuatroTest {
 
         }
         assertEquals(false, jugador.tieneCasoAsignado());
+        */
+
     }
 
 
